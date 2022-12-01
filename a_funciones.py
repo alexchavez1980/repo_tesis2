af55bde30de5f79bf862ccf85144b10622749cf9
alexchavez1980 <alexchavez1980@gmail.com>
2022-12-01 17:35:56 -0300

    01/Dic

--- a_funciones.py
+++ a_funciones.py
@@ -1,14 +1,15 @@
+#-----------------------------------------------------------------------
+# Librerías y algunas variables
+#-----------------------------------------------------------------------
 import mne                                                              # pip install mne
 mne.set_log_level('WARNING')                                            # Luego averiguar ¿para qué?
 import scipy.io
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sb                                                    # Por ahora lo voy a usar para cambiar los nombres de las columnas
-color = ['green', 'blue','red','cyan', 'magenta', 'yellow','k','w']     # Paleta de colores para diferenciar las ondas
-
+import seaborn as sb                                                    # Por ahora lo voy a usar para cambiar los nombres de las columnas#-----------------------------------------------------------------------
 #-----------------------------------------------------------------------
-# Mis funciones
+# # Funciones
 #-----------------------------------------------------------------------
 def to_df(no_df):
     df = pd.DataFrame(no_df) 
@@ -67,7 +68,7 @@ def grafic_signal(data_frame, titulo, xlabel, ylabel, dir_savefig, canal):
     plt.show()
 
 
-def grafico3(data_frame, titulo, xlabel, ylabel, dir_savefig, canal):          # Función para graficar canales por separado. Con 'index'
+def grafic_1ch(data_frame, titulo, xlabel, ylabel, dir_savefig, canal):          # Función para graficar canales por separado. Con 'index'
     plt.figure(figsize=(30,8))                                          # Acá no hay for. OJO.
     axes = plt.gca()
     plt.plot(data_frame['index'], data_frame[canal], color[1])
@@ -85,4 +86,42 @@ def grafico3(data_frame, titulo, xlabel, ylabel, dir_savefig, canal):          #
 def sumemos(a,b,c):
     d = a+b+c
     return d    
+
+
+#----------------------------------------------------------------------
+# Algunas funciones extra que uso.
+#----------------------------------------------------------------------
+def grafico1(data_frame, titulo, xlabel, ylabel, dir_savefig):          # Función para graficar los ocho canales. Con 'index'
+    plt.figure(figsize=(30,8))                                          # Se usa para signal, tirals o flash, que vienen de mat['data'][0][0][X] 
+    axes = plt.gca()
+    for i in range(0,7):
+        plt.plot(data_frame['index'], data_frame[i], color[i])
+    axes.set_title(titulo)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.title.set_size(30)
+    axes.xaxis.label.set_size(20)
+    axes.yaxis.label.set_size(20)
+    plt.axis((0,350000,-1700,2000))                                     #plt.axis((eje_ploteo))
+    plt.grid()
+    plt.savefig(dir_savefig)
+    plt.show()
+    return()   
+
+def grafico2(data_frame, titulo, xlabel, ylabel, dir_savefig):          # Función para graficar los ocho canales. Con 'Sample'
+    plt.figure(figsize=(30,8))                                          # Se usa para erptemplate, que es mat['routput'].
+    axes = plt.gca()                                                    # Revisar ¿por qué?
+    for i in range(0,7):
+        plt.plot(data_frame['sample'], data_frame[i], color[i])
+    axes.set_title(titulo)
+    axes.set_xlabel(xlabel)
+    axes.set_ylabel(ylabel)
+    axes.title.set_size(30)
+    axes.xaxis.label.set_size(20)
+    axes.yaxis.label.set_size(20)
+    plt.axis((0,250,-4,7))                                              #plt.axis((eje_ploteo))
+    plt.grid()
+    plt.savefig(dir_savefig)
+    plt.show()
+    return()   
 #-----------------------------------------------------------------------
